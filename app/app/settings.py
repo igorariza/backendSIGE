b4b104981f231bc9bf549325ce19607a2ca675e9
Luis <luis.alberto.gomez@correounivalle.edu.co>
2020-04-13 05:05:04 -0500

    adding a configuration by postgresSQL DB

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,8 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
-    'localhost',
-    'sige-api-groupar.herokuapp.com'
+    'localhost'
 ]
 
 
@@ -47,7 +46,8 @@ INSTALLED_APPS = [
     'test',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders'
+    'corsheaders',
+    'psycopg2'
 ]
 
 MIDDLEWARE = [
@@ -84,13 +84,25 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sige2020',
+        'USER': 'adminsige',
+        'PASSWORD': 'ia2127374',
+        'HOST': 'sige-2020.cfgbol9pangg.sa-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}"""
 
 
 # Password validation
