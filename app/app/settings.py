099210e3184a03e62ff3c29ee845b694ad43a853
Igor Ariza <igorariza@gmail.com>
2020-04-28 19:37:03 -0500

    deploy: V: 0.0.2

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
     'localhost',
-    'ec2-52-67-254-116.sa-east-1.compute.amazonaws.com',
-    '52.67.254.116'
+    'ec2-18-230-107-229.sa-east-1.compute.amazonaws.com',
+    '18.230.107.229'
 ]
 
 
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -109,15 +109,15 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""       
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -174,3 +174,4 @@ REST_AUTH_REGISTER_SERIALIZERS = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
