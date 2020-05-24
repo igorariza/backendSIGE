f5013b8cb6be8d5f1c2503802de0ef47cd3fce10
Igor Ariza <igorariza@gmail.com>
2020-05-23 20:54:55 -0500

    feat: s3 init

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,15 +27,7 @@ ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
     'localhost',
-<<<<<<< HEAD
-    'ec2-18-229-136-215.sa-east-1.compute.amazonaws.com',
-    'ec2-18-230-107-229.sa-east-1.compute.amazonaws.com',
-    'ec2-18-230-128-152.sa-east-1.compute.amazonaws.com',
-    '18.230.128.152',
-    '18.230.107.229',
-=======
-    'ec2-177-71-141-32.sa-east-1.compute.amazonaws.com',
->>>>>>> 3454a45c0357610dfae43b2711a2afd39606416d
+    'ec2-54-94-141-235.sa-east-1.compute.amazonaws.com',
     'api.sige-edu.com'
 ]
 
