06994adb8161d4a2539d1d204147458b63a327dd
Igor Ariza <igorariza@gmail.com>
2020-05-24 12:00:15 -0500

    feat: storage s3

--- app/files/admin.py
+++ app/files/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import PhotoUser
-# Register your models here.
-admin.site.register(PhotoUser)
+# from .models import PhotoUser
+# # Register your models here.
+# admin.site.register(PhotoUser)
