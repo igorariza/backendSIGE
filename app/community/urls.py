cfd4326855d122618fb8744f329fb7d7b22525db
Igor Ariza <igorariza@gmail.com>
2020-07-16 23:08:48 -0500

    feat: update 16-06

--- app/community/urls.py
+++ app/community/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('create/', CommunityCreate.as_view()),
     path('', CommunityList.as_view()),
     path('<pk>', CommunityDetail.as_view()),
-    #path('delete/<pk>', CommunityDelete.as_view()),
+    path('delete/<pk>', CommunityDelete.as_view()),
     path('upate/<pk>', CommunityUpdate.as_view()),
     path('byIE/<codeIE>', CommunityListbyIE.as_view())
     ]
