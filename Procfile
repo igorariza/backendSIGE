45a9135936f58069e9e98d4205bc5ebe4b76f456
Igor Ariza <igorariza@gmail.com>
2020-10-22 01:14:43 -0500

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT backend:application
+web: gunicorn --bind 0.0.0.0:$PORT wsgi:app
--- backend.py
+++ backend.py
@@ -6,4 +6,4 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='18.230.107.229')
+    application.run()
