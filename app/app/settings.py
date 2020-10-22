07cf4f093f492433fcee818dee1ecb1b81f1a3c5
Igor Ariza <igorariza@gmail.com>
2020-10-22 13:57:50 -0500

    make it better

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,7 +76,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
